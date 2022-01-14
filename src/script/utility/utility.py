04853bf4f3f67a79808a0017a7126a6a4d10d242
Kaydo <kaydo1@live.nl>
2022-01-14 22:43:09 +0100

    Finished the automization process but the final plot is not yet correct.

--- src/script/utility/utility.py
+++ src/script/utility/utility.py
@@ -30,9 +30,12 @@ def slice_string_at_char(str, char):
 
 def get_metadata_item(metadata, item):
     return slice_string_at_char(elements_containing_string(item, metadata)[0], '=')[1]
-
+    
 def err_period(frequencies):
     frequency_mean = np.mean(frequencies)
     frequency_std = np.std(frequencies)
     error = (frequency_std)/(frequency_mean**2)
     return error
+
+def period_and_error(frequencies):
+    return 1 / (mean := np.mean(frequencies)), np.std(frequencies) / mean**2
